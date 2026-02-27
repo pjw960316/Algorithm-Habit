61d6126ce6e1be5df474ff1591437d29a2d8e205
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2026-02-27 15:50:33 +0900

    [Unrated] Title: 정ㅋ벅ㅋ, Time: 40 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Unrated/1237. 정ㅋ벅ㅋ/정ㅋ벅ㅋ.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class CSharpHabit
+{
+    static void Main()
+    {
+        // TODO : Console.ReadLine() 또는 Console.ReadLine().Split()
+        Console.Write("문제의 정답");        
+    }
+}
