89ac66cd14314b9a23936d578edd1a646df41051
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2026-03-27 15:29:39 +0900

    [Gold IV] Title: 카드 섞기, Time: 344 ms, Memory: 45116 KB -BaekjoonHub

---
+++ 백준/Gold/1091. 카드 섞기/카드 섞기.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+public class CSharpHabit
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        var pArr = new int[48]; //카드 넘버 -> 위치
+        var sArr = new int[48]; //위치 -> 카드 넘버
+
+        var line = Console.ReadLine().Split();
+        for (int idx = 0; idx < n; idx++)
+        {
+            pArr[idx] = int.Parse(line[idx]);
+        }
+        
+        line = Console.ReadLine().Split();
+        for (int idx = 0; idx < n; idx++)
+        {
+            sArr[idx] = int.Parse(line[idx]);
+        }
+        
+        var arr = new int[48]; //위치 -> 카드 넘버
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = i;
+        }
+        var originArr = arr.Clone() as int[];
+
+        var hashSet = new HashSet<string>();
+        int count = 0;
+        while (true)
+        {
+            if (IsDuplicate())
+            {
+                Console.Write(-1);
+                return;
+            }
+            
+            if (Verify())
+            {
+                Console.Write(count);
+                return;
+            }
+            arr = Mix();
+
+            count++;
+        }
+        
+        int[] Mix()
+        {
+            var retArr = new int[48];
+            retArr = arr.Clone() as int[];
+
+            for (int idx = 0; idx < n; idx++)
+            {
+                var targetIdx = sArr[idx];
+                retArr[targetIdx] = arr[idx];
+            }
+
+            return retArr;
+        }
+
+        bool Verify()
+        {
+            for (int cardNum = 0; cardNum < n; cardNum++)
+            {
+                for (int idx = 0; idx < n; idx++)
+                {
+                    if (arr[idx] == cardNum)
+                    {
+                        if (idx % 3 != pArr[cardNum] % 3)
+                        {
+                            return false;
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        bool IsDuplicate()
+        {
+            var sb = new StringBuilder();
+            for (int idx = 0; idx < n; idx++)
+            {
+                sb.Append(arr[idx]);
+            }
+
+            var str = sb.ToString();
+            
+            if (hashSet.Contains(str))
+            {
+                return true;
+            }
+            else
+            {
+                hashSet.Add(str);
+                return false;
+            }
+        }
+    }
+}
