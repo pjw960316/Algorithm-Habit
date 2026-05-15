c32bfb6b60eeb05eb8125c83f7d59c455012823b
JIWON PARK <55792986+pjw960316@users.noreply.github.com>
2026-05-15 15:48:14 +0900

    디펜스 게임

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cs
@@ -0,0 +1,75 @@
+﻿public class Solution
+{
+    private int[] _enemyArr;
+    private int _skipCount;
+    private int _curSoldier;
+    
+    private PriorityQueue<int, int> _pq = new PriorityQueue<int, int>();
+    
+    public int solution(int n, int k, int[] enemy)
+    {
+        var answer = 0;
+
+        _curSoldier = n;
+        _skipCount = k;
+        _enemyArr = new int[enemy.Length + 2];
+        
+        for (int idx = 0; idx < enemy.Length; idx++)
+        {
+            _enemyArr[idx + 1] = enemy[idx];
+        }
+        
+        for(int idx=1; idx<=enemy.Length; idx++)
+        {
+            if (IsOkay(_enemyArr[idx]))
+            {
+                answer++;
+            }
+            else
+            {
+                return answer;
+            }
+        }
+
+        return answer;
+    }
+
+    private bool IsOkay(int element)
+    {
+        if (_pq.Count < _skipCount)
+        {
+            _pq.Enqueue(element,element);
+            
+            return true;
+        }
+        
+        if (_pq.Peek() < element)
+        {
+            _curSoldier -= _pq.Dequeue();
+            _pq.Enqueue(element,element);
+        }
+        else
+        {
+            _curSoldier -= element;
+        }
+
+        if (_curSoldier < 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var sol = new Solution();
+
+        // TODO
+        // Static Input Class의 메서드를 활용해서 solution 메서드에 인자를 넣으세요.
+        Console.Write(sol.solution(Input.ReadInt(), Input.ReadInt(), Input.ReadInts()));
+    }
+}
